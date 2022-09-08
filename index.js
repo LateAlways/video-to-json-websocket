0b1cb4382b2e401c88292c4364f802844952bf17
LateAlways <110630690+LateAlways@users.noreply.github.com>
2022-09-08 19:16:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,104 +7,103 @@ const Jimp = require('jimp');
 const fs = require("fs");
 const WebSocket = require('ws');
 
-if(!filesys.exists("./request")) {
-   filesys.makefolder("./request")
+if (!filesys.exists("./request")) {
+    filesys.makefolder("./request")
 }
 
-if(!filesys.exists("./saves")) {
-   filesys.makefolder("./saves")
+if (!filesys.exists("./saves")) {
+    filesys.makefolder("./saves")
 }
 
 const server = new WebSocket.Server({
-  port: process.env.PORT
+    port: process.env.PORT || 4000
 });
 
 let sockets = [];
 server.on('connection', async function(socket) {
-  sockets.push(socket);
+    sockets.push(socket);
     console.log("New Connection")
-  // When you receive a message, send that message to every socket.
-  socket.on('message', function(msg) {
-    const { action, video, frame } = JSON.parse(((Buffer.from(msg, 'base64')).toString("utf-8")));
-    const url = video;
-    console.log("Request: "+action + ", " + video + ", " + frame)
-    const promise = new Promise((resolve, reject) => {
-        if(filesys.exists("./request/"+url.replace("https://www.youtube.com/watch?v=","")+".mp4")) {
-    		if(!filesys.exists("./saves/"+url.replace("https://www.youtube.com/watch?v=","")+"/")) {
-    		    filesys.makefolder("./saves/"+url.replace("https://www.youtube.com/watch?v=","")+"/");
-    		}
-    		
-    		ffmpeg("./request/"+url.replace("https://www.youtube.com/watch?v=","")+".mp4")
-    		    .on("end", ()=> {
-    		        console.log("done")
-    		        resolve();
-    		    })
-    		    .on("error", (err) => {
-    		        reject(err);
-    		    })
-    		    .seek(frame / 1000)
-    		    .outputOptions(["-vframes",1,"-q:v",2])
-    		    .output('./saves/'+url.replace("https://www.youtube.com/watch?v=","")+"/"+url.replace("https://www.youtube.com/watch?v=","")+frame.toString()+'.jpg')
-    		    .run();
-    		/*extractFrame({
-    			input: "./request/"+url.replace("https://www.youtube.com/watch?v=","")+".mkv",
-    			output: './saves/'+url.replace("https://www.youtube.com/watch?v=","")+"/"+url.replace("https://www.youtube.com/watch?v=","")+"-"+frame.toString()+'.jpg',
-    			offset: frame
-    		})*/
-        } else {
-            ytdl(url).on("end",() => {
-                ffmpeg("./request/"+url.replace("https://www.youtube.com/watch?v=","")+".mp4")
-                .on("error", (err) => {
-    		        reject(err);
-    		    })
+        // When you receive a message, send that message to every socket.
+    socket.on('message', function(msg) {
+        const {
+            action,
+            video,
+            frame
+        } = JSON.parse(((Buffer.from(msg, 'base64')).toString("utf-8")));
+        const url = video;
+        console.log("Request: " + action + ", " + video + ", " + frame)
+        if (filesys.exists("./request/" + url.replace("https://www.youtube.com/watch?v=", "") + ".mkv")) {
+            if (!filesys.exists("./saves/" + url.replace("https://www.youtube.com/watch?v=", "") + "/")) {
+                filesys.makefolder("./saves/" + url.replace("https://www.youtube.com/watch?v=", "") + "/");
+            }
+
+            ffmpeg("./request/" + url.replace("https://www.youtube.com/watch?v=", "") + ".mkv")
                 .on("end", () => {
-                    if(!filesys.exists("./saves/"+url.replace("https://www.youtube.com/watch?v=",""))) {
-            		    filesys.makefolder("./saves/"+url.replace("https://www.youtube.com/watch?v=",""));
-            		}
-        			ffmpeg("./request/"+url.replace("https://www.youtube.com/watch?v=","")+".mp4")
-            		    .on("end", ()=> {
-            		        resolve();
-            		    })
-            		    .on("error", (err) => {
-            		        reject(err);
-            		    })
-            		    .seek(frame / 1000)
-            		    .outputOptions(["-vframes",1,"-q:v",2])
-            		    .output('./saves/'+url.replace("https://www.youtube.com/watch?v=","")+"/"+url.replace("https://www.youtube.com/watch?v=","")+frame.toString()+'.jpg')
-            		    ;
-        		})
-        		.inputFPS(20)
-        		
-        		.pipe(fs.createWriteStream("./request/"+url.replace("https://www.youtube.com/watch?v=","")+".mp4"));
-            }).pipe(fs.createWriteStream("./request/"+url.replace("https://www.youtube.com/watch?v=","")+".mp4"));
+                    console.log("Done");
+                    Jimp.read('./saves/' + url.replace("https://www.youtube.com/watch?v=", "") + "/" + url.replace("https://www.youtube.com/watch?v=", "") + frame.toString() + '.png')
+                        .then(image => {
+                            var width = image.bitmap.width;
+                            var height = image.bitmap.height;
+                            var pixels = [];
+                            for (var y = 0; y < height; y++) {
+                                var rowPixels = [];
+                                for (var x = 0; x < width; x++) {
+                                    var pixel = Jimp.intToRGBA(image.getPixelColor(x, y));
+                                    rowPixels.push(`${pixel.r}, ${pixel.g}, ${pixel.b}`);
+                                }
+                                pixels.push(rowPixels);
+                            }
+                            socket.send(JSON.stringify({
+                                data: pixels
+                            }))
+                        });
+                })
+                .seek(frame / 1000)
+                .outputOptions(["-vframes", 1, "-q:v", 2])
+                .output('./saves/' + url.replace("https://www.youtube.com/watch?v=", "") + "/" + url.replace("https://www.youtube.com/watch?v=", "") + frame.toString() + '.png')
+                .run();
+        } else {
+            ytdl(url).on("end", () => {
+                ffmpeg("./request/" + url.replace("https://www.youtube.com/watch?v=", "") + ".mkv")
+                    .on("end", () => {
+                        if (!filesys.exists("./saves/" + url.replace("https://www.youtube.com/watch?v=", ""))) {
+                            filesys.makefolder("./saves/" + url.replace("https://www.youtube.com/watch?v=", ""));
+                        }
+                        ffmpeg("./request/" + url.replace("https://www.youtube.com/watch?v=", "") + ".mkv")
+                            .on("end", () => {
+                                Jimp.read('./saves/' + url.replace("https://www.youtube.com/watch?v=", "") + "/" + url.replace("https://www.youtube.com/watch?v=", "") + frame.toString() + '.png')
+                                    .then(image => {
+                                        var width = image.bitmap.width;
+                                        var height = image.bitmap.height;
+                                        var pixels = [];
+                                        for (var y = 0; y < height; y++) {
+                                            var rowPixels = [];
+                                            for (var x = 0; x < width; x++) {
+                                                var pixel = Jimp.intToRGBA(image.getPixelColor(x, y));
+                                                rowPixels.push(`${pixel.r}, ${pixel.g}, ${pixel.b}`);
+                                            }
+                                            pixels.push(rowPixels);
+                                        }
+                                        socket.send(JSON.stringify({
+                                            data: pixels
+                                        }))
+                                    });
+                            })
+                            .on("error", (err) => {})
+                            .seek(frame / 1000)
+                            .outputOptions(["-vframes", 1, "-q:v", 2])
+                            .output('./saves/' + url.replace("https://www.youtube.com/watch?v=", "") + "/" + url.replace("https://www.youtube.com/watch?v=", "") + frame.toString() + '.png');
+                    })
+                    .inputFPS(20)
+					.pipe(fs.createWriteStream("./request/" + url.replace("https://www.youtube.com/watch?v=", "") + ".mkv"));
+            }).pipe(fs.createWriteStream("./request/" + url.replace("https://www.youtube.com/watch?v=", "") + ".mkv"));
         }
     });
-    //END PROMISE
-    promise.then(() => {
-        Jimp.read('./saves/'+url.replace("https://www.youtube.com/watch?v=","")+"/"+url.replace("https://www.youtube.com/watch?v=","")+frame.toString()+'.jpg')
-            .then(image => {
-            var width = image.bitmap.width;
-            var height = image.bitmap.height;
-            var pixels = [];
-            for (var y = 0; y < height; y++) {
-                var rowPixels = [];
-                for (var x = 0; x < width; x++) {
-                var pixel = Jimp.intToRGBA(image.getPixelColor(x, y));
-                rowPixels.push(`${pixel.r}, ${pixel.g}, ${pixel.b}`);
-                }
-                pixels.push(rowPixels);
-            }
-            socket.send(((Buffer.from(JSON.stringify({ data: pixels }),"utf-8")).toString("base64")))
-        });
-    }, (err) => {
-        console.log(err);
-    });
-  });
 
-  // When a socket closes, or disconnects, remove it from the array.
-  socket.on('close', function() {
-    sockets = sockets.filter(s => s !== socket);
-    console.log("Connection Disconnected")
-  });
+    // When a socket closes, or disconnects, remove it from the array.
+    socket.on('close', function() {
+        sockets = sockets.filter(s => s !== socket);
+        console.log("Connection Disconnected")
+    });
 });
 console.log("Server running on port 443");
